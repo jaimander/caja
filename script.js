701483b3efc3e6dd8dd822b0e4d972b75dbd2c90
Jaime Andrés Rodriguez Gómez <jaimerodriguezgo@gmail.com>
2025-10-22 14:45:38 -0500

    animacion 4 lados

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 let puertaIzq = document.getElementById("puertaIzq");
 let puertaDer = document.getElementById("puertaDer");
+let puertaArr = document.getElementById("puertaArr");
+let puertaAba = document.getElementById("puertaAba");
+
 let chapaIzq = document.getElementById("chapaIzq");
 let chapaDer = document.getElementById("chapaDer");
+
+puertaIzq.classList.add("cerrar");
+puertaDer.classList.add("cerrar");
+puertaArr.classList.add("cerrar");
+puertaAba.classList.add("cerrar");
 
 puertaIzq.onclick = function () {
   if (puertaIzq.classList.contains("abrir")) {
@@ -20,5 +28,25 @@ puertaDer.onclick = function () {
   } else {
     puertaDer.classList.remove("cerrar");
     puertaDer.classList.add("abrir");
+  }
+};
+
+puertaArr.onclick = function () {
+  if (puertaArr.classList.contains("abrir")) {
+    puertaArr.classList.remove("abrir");
+    puertaArr.classList.add("cerrar");
+  } else {
+    puertaArr.classList.remove("cerrar");
+    puertaArr.classList.add("abrir");
+  }
+};
+
+puertaAba.onclick = function () {
+  if (puertaAba.classList.contains("abrir")) {
+    puertaAba.classList.remove("abrir");
+    puertaAba.classList.add("cerrar");
+  } else {
+    puertaAba.classList.remove("cerrar");
+    puertaAba.classList.add("abrir");
   }
 };
